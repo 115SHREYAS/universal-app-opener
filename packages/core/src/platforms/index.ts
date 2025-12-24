c59594f309f409f916fa7eb5b26a2a687467223f
tanmoyPaul12 <tanmoy01paul@gmail.com>
2025-12-24 21:09:47 +0530

    chore: remove unrelated reddit changes

--- packages/core/src/platforms/index.ts
+++ packages/core/src/platforms/index.ts
@@ -2,10 +2,12 @@ import { linkedinHandler } from "./linkedin";
 import { unknownHandler } from "./unknown";
 import { youtubeHandler } from "./youtube";
 import { instagramHandler } from "./instagram";
+import { threadsHandler } from "./threads";
 
 export {
     linkedinHandler,
     youtubeHandler,
     instagramHandler,
+    threadsHandler,
     unknownHandler
 }
